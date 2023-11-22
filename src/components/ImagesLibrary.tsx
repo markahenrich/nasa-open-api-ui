cc469ccab03a4455f7850b7f9ba24daf7d0123a9
Mark Henrich <mark.henrich@icloud.com>
2023-11-22 16:01:05 -0700

    Add map to display images returned from api

--- src/components/ImagesLibrary.tsx
+++ src/components/ImagesLibrary.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { getSearchResults } from "../services/imageslibrary.service";
+import { Image } from "../types/imageslibrary.type";
 
 const ImagesLibrary = () => {
+    const [images, setImages] = useState<Image[] | null | undefined>(null);
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -8,14 +11,26 @@ const ImagesLibrary = () => {
         const imgSearch = formData.get('img-search')?.toString();
         if (!imgSearch) return; 
         const searchResults = await getSearchResults(imgSearch); 
-        console.log(searchResults);
+        setImages(searchResults?.collection.items);
+        console.log(images);
     }
 
     return(
-        <form onSubmit={(e) => handleSubmit(e)}>
-            <input type="text" name="img-search" placeholder=""></input>
-            <button type="submit">Search</button>
-        </form>
+        <>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" name="img-search" placeholder=""></input>
+                <button type="submit">Search</button>
+            </form>
+            {
+                images?.map((image) => {
+                    return (
+                        <>
+                            <img src={image.links?.length > 0 ? image.links[0].href : ''} alt=""/>
+                        </>
+                    )
+                })
+            }
+        </>
     );
 }
 
